ce9ae25c567528fbe498280986c529e1887cbb01
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-10-17 23:38:02 +0500

    update

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -2,3 +2,9 @@
 import { useState } from "react";
 import { useMutation } from "convex/react";
 import { useParams } from "next/navigation";
+
+import {
+  Dialog,
+  DialogContent,
+  AlertDialogHeader,
+} from "@/components/ui/dialog";
