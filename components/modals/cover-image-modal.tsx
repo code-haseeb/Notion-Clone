44fc16397807ef70d6d5067ab534d5128ac64935
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-10-17 23:45:20 +0500

    update

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -3,8 +3,17 @@ import { useState } from "react";
 import { useMutation } from "convex/react";
 import { useParams } from "next/navigation";
 
+import { useCoverImage } from "@/hooks/use-cover-image";
+import { SingleImageDropzone } from "@/components/single-image-dropzone";
 import {
   Dialog,
   DialogContent,
   AlertDialogHeader,
 } from "@/components/ui/dialog";
+
+export const CoverImageModal = () => {
+  const params = useParams();
+  const update = useMutation(api.documents.update);
+  const coverImage = useCoverImage();
+  const edgeStore = useEdgeStore();
+};
