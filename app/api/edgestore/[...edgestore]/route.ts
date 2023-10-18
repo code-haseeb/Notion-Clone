cc5a016d6f00e23b3e3484e95541038f17e50884
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-10-18 09:16:16 +0500

    update

--- app/api/edgestore/[...edgestore]/route.ts
+++ app/api/edgestore/[...edgestore]/route.ts
@@ -1,14 +1,18 @@
-import {initEdgeStore} from "@edgestore/server"
-import {createEdgeStoreNextHandler } from "@edgestore/server/adapters/next/app"
+import { initEdgeStore } from "@edgestore/server";
+import { createEdgeStoreNextHandler } from "@edgestore/server/adapters/next/app";
 
 const es = initEdgeStore.create();
 
-const edgeStoreRouter = es.router({publicFiles:es.fileBucket().beforeDelete(()=>{return true})})
+const edgeStoreRouter = es.router({
+  publicFiles: es.fileBucket().beforeDelete(() => {
+    return true;
+  }),
+});
 
 const handler = createEdgeStoreNextHandler({
-    router:edgeStoreRouter,
-})
+  router: edgeStoreRouter,
+});
 
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
 
-export type EdgeStoreRouter = type edgeStoreRouter;
+export type EdgeStoreRouter = typeof edgeStoreRouter;
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://descriptive-hawk-740.convex.cloud",
+      applicationID: "convex",
+    },
+  ],
+};
