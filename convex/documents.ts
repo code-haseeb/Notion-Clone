972bc85f5b964e440a2d1fb3d41754162e97080c
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-10-18 11:06:29 +0500

    update

--- convex/documents.ts
+++ convex/documents.ts
@@ -254,3 +254,51 @@ export const update = mutation({
     return document;
   },
 });
+
+export const removeIcon = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("NoT Authenticated");
+    }
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+    if (!existingDocument) {
+      throw new Error("Not Found");
+    }
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unautherized");
+    }
+    const document = await ctx.db.patch(args.id, {
+      icon: undefined,
+    });
+    return document;
+  },
+});
+
+export const removeCoverImage = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("NoT Authenticated");
+    }
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+    if (!existingDocument) {
+      throw new Error("Not Found");
+    }
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unautherized");
+    }
+    const document = await ctx.db.patch(args.id, {
+      coverImage: undefined,
+    });
+    return document;
+  },
+});
