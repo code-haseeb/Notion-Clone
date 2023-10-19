93fcffb75459d27add2ea633fdfd6e295324b826
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-10-19 12:02:40 +0500

    update

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,3 +1,8 @@
 "use client ";
 
 import { useQuery, useMutation } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+import { Cover } from "@/components/cover";
+import { Toolbar } from "@/components/toolbar";
+import { Id } from "@/convex/_generated/dataModel";
