4eac11afc99b93190f70978b873b733b4cb62a87
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-10-21 22:22:37 +0500

    updade

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
-import "./globals.css";
+import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
