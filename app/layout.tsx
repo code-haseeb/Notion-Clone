6a9846e7f1d1083ed2e5f70f08bd2141f061d80a
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-10-22 11:24:31 +0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import { Toaster } from "sonner";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import type { Metadata } from "next";
 
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { EdgeStoreProvider } from "@/lib/edgestore";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
 export const metadata: Metadata = {
   title: "Jotion",
   description: "The connected workspace where better, faster work happens.",
@@ -31,10 +35,25 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // Nothing Interesting Here
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ConvexClientProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+              storageKey="jotion-theme-2"
+            >
+              <Toaster position="bottom-center" />
+              <ModalProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
+        </ConvexClientProvider>
+      </body>
     </html>
   );
 }
