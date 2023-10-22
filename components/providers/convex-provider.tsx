b149ffb7ccad65a7e4c298b7872d7bdff7336cea
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-10-22 11:29:37 +0500

    update

--- components/providers/convex-provider.tsx
+++ components/providers/convex-provider.tsx
@@ -1,75 +1,20 @@
 "use client";
 
-import Image from "next/image";
-import { ImageIcon, X } from "lucide-react";
-import { useMutation } from "convex/react";
-import { useParams } from "next/navigation";
+import { ReactNode } from "react";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 
-import { Skeleton } from "@/components/ui/skeleton";
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import { useCoverImage } from "@/hooks/use-cover-image";
-import { api } from "@/convex/_generated/api";
-import { Id } from "@/convex/_generated/dataModel";
-import { useEdgeStore } from "@/lib/edgestore";
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-interface CoverImageProps {
-  url?: string;
-  preview?: boolean;
-}
-
-export const Cover = ({ url, preview }: CoverImageProps) => {
-  const { edgestore } = useEdgeStore();
-  const params = useParams();
-  const coverImage = useCoverImage();
-  const removeCoverImage = useMutation(api.documents.removeCoverImage);
-
-  const onRemove = async () => {
-    if (url) {
-      await edgestore.publicFiles.delete({
-        url: url,
-      });
-    }
-    removeCoverImage({
-      id: params.documentId as Id<"documents">,
-    });
-  };
-
+export const ConvexClientProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <div
-      className={cn(
-        "relative w-full h-[35vh] group",
-        !url && "h-[12vh]",
-        url && "bg-muted"
-      )}
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
     >
-      {!!url && <Image src={url} fill alt="Cover" className="object-cover" />}
-      {url && !preview && (
-        <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
-          <Button
-            onClick={() => coverImage.onReplace(url)}
-            className="text-muted-foreground text-xs"
-            variant="outline"
-            size="sm"
-          >
-            <ImageIcon className="h-4 w-4 mr-2" />
-            Change cover
-          </Button>
-          <Button
-            onClick={onRemove}
-            className="text-muted-foreground text-xs"
-            variant="outline"
-            size="sm"
-          >
-            <X className="h-4 w-4 mr-2" />
-            Remove
-          </Button>
-        </div>
-      )}
-    </div>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
   );
-};
-
-Cover.Skeleton = function CoverSkeleton() {
-  return <Skeleton className="w-full h-[12vh]" />;
 };
